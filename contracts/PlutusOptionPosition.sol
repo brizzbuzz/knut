eee0f6bd1b9874b53159b4adf28d831622c24304
Ryan Brink <ryanbrink@protonmail.com>
2020-10-08 07:00:17 -0400

    exercise v0.1

--- contracts/PlutusOptionPosition.sol
+++ contracts/PlutusOptionPosition.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract PlutusOptionPosition is ERC721, Ownable {
 
   struct Position {
+    // TODO Address
     uint256 cost;
     uint256 value;
   }
@@ -30,10 +31,26 @@ contract PlutusOptionPosition is ERC721, Ownable {
     return nextPopId;
   }
 
+
+  function burn(uint256 tokenID) public {
+    // TODO
+  }
+
   function checkPosition(uint256 tokenId) public view returns (uint256, uint256) {
     require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
     // todo better to do?? -> Position memory position = _Positions[tokenId];
     return (_Positions[tokenId].value, _Positions[tokenId].cost);
+  }
+
+  function checkPositionValue(uint256 tokenId) public view returns (uint256) {
+    require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+    return _Positions[tokenId].value;
+  }
+
+  function checkPositionCost(uint256 tokenId) public view returns (uint256) {
+    // TODO Make a modifier for this?
+    require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+    return _Positions[tokenId].cost;
   }
 
   function _setPosition(uint256 tokenId, uint256 _cost, uint256 _value) private {
