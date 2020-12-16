56c3075c46ecac1884f0e70a35821f2f94c8bc64
Ryan Brink <ryanbrink@protonmail.com>
2020-12-15 19:16:31 -0500

    vvv nice

--- src/test/kotlin/org/leafygreens/knut/generated/contracts/GringottsTest.kt
+++ src/test/kotlin/org/leafygreens/knut/generated/contracts/GringottsTest.kt
@@ -12,10 +12,12 @@ import org.web3j.abi.FunctionEncoder
 import org.web3j.abi.datatypes.Address
 import org.web3j.abi.datatypes.Function
 import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.DefaultBlockParameterName
 import org.web3j.protocol.core.methods.request.Transaction
 import org.web3j.tx.TransactionManager
 import org.web3j.tx.gas.ContractGasProvider
 import org.web3j.tx.gas.DefaultGasProvider
+import org.web3j.utils.Convert
 
 @EVMTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -37,21 +39,26 @@ class GringottsTest {
   }
 
   @Test
-  internal fun `Gringotts mints knuts and creates an unbreakable vow`(web3j: Web3j) {
+  internal fun `Gringotts mints knuts and creates an unbreakable vow`(web3j: Web3j, contractGasProvider: ContractGasProvider) {
     val creds = generateFundedCreds(BigDecimal.ONE, web3j)
+    val startingBalance = web3j.ethGetBalance(creds.address, DefaultBlockParameterName.LATEST).send().balance
+
     val lockupFunction = Function(
         "lockup",
         listOf(Address(creds.address)),
         emptyList()
     )
+
     val encodedFunction = FunctionEncoder.encode(lockupFunction)
+    val amountToLockup = Convert.toWei("0.5", Convert.Unit.ETHER)
+
     val tx = Transaction.createFunctionCallTransaction(
         creds.address,
         BigInteger.ZERO,
         DefaultGasProvider.GAS_PRICE,
         DefaultGasProvider.GAS_LIMIT,
         gringotts.contractAddress,
-        BigInteger.valueOf(10000),
+        amountToLockup.toBigInteger(),
         encodedFunction
     )
 
@@ -64,13 +71,21 @@ class GringottsTest {
       Thread.sleep(100)
     } while (web3j.ethGetTransactionReceipt(txHash).send().transactionReceipt.isEmpty)
 
+    val gasCost = web3j.ethGetTransactionReceipt(txHash).send().transactionReceipt.get().gasUsed
+        .times(contractGasProvider.gasPrice)
+
+    val newBalance = startingBalance
+        .minus(amountToLockup.toBigInteger())
+        .minus(gasCost)
+
     assertEquals(BigInteger.valueOf(500), knut.balanceOf(creds.address).send())
-    assertEquals(BigInteger.valueOf(10000), vows.checkPosition(BigInteger.ONE).send().component1())
+    assertEquals(amountToLockup.toBigInteger(), vows.checkPosition(BigInteger.ONE).send().component1())
     assertEquals(BigInteger.valueOf(500), vows.checkPosition(BigInteger.ONE).send().component2())
+    assertEquals(newBalance, web3j.ethGetBalance(creds.address, DefaultBlockParameterName.LATEST).send().balance)
   }
 
-  @Test
-  internal fun `Gringotts allows positions to be exercised`() {
-    TODO("Not yet implemented")
-  }
+//  @Test
+//  internal fun `Gringotts allows positions to be exercised`() {
+//    TODO("Not yet implemented")
+//  }
 }
